de0a460984cd1c3f583c7b550f361853564e2555
Nikhil Nishane <139981515+phobicnikk@users.noreply.github.com>
2024-04-10 19:22:10 +0530

    Update gas-station.java

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,20 +1,19 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int n = cost.length;
-        int deficit = 0; // Corrected variable name
+        int deficit = 0; 
         int balance = 0;
         int start = 0;
 
         for(int i = 0; i < n; i++) {
-            balance += gas[i] - cost[i]; // Corrected calculation of balance
-
+            balance += gas[i] - cost[i]; 
             if(balance < 0) {
                 deficit += balance;
                 start = i + 1;
                 balance = 0;
             }
         }
-        if(balance + deficit >= 0) // Corrected condition for checking if a solution exists
+        if(balance + deficit >= 0) 
             return start;
         return -1;
     }
