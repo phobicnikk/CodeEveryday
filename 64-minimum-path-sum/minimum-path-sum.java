1bd3706d049e86649f58bf64a209cd86a4b4dde4
Nikhil Nishane <139981515+phobicnikk@users.noreply.github.com>
2024-06-01 22:32:59 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (54.42%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,22 +1,17 @@
 class Solution {
-    public int minPathSum(int[][] cost) {
-        int m = cost.length;
-        int n = cost[0].length;
-        int[][] dp = new int[m][n];
-        dp[0][0] = cost[0][0];
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = dp[i - 1][0] + cost[i][0];
-        }
-        for (int i = 1; i < n; i++) {
-            dp[0][i] = dp[0][i - 1] + cost[0][i];
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = cost[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-        return dp[m - 1][n - 1];
+    public int minPathSum(int[][] grid) {
+       int m= grid.length;
+        int n=grid[0].length;
+        int memo[][]=new int[m][n];
+        return find(grid,m-1,n-1,memo);  
     }
-
+    public static int find(int [][]grid,int m,int n,int memo[][]){
+        if (m==0&&n==0) return grid[m][n];
+        else if (m<0||n<0) {
+            return Integer.MAX_VALUE;
+        } else if (memo[m][n]!=0) {
+            return memo[m][n];
+        }
+        return memo[m][n]=grid[m][n]+Math.min(find(grid,m-1,n,memo),find(grid,m,n-1,memo));
+    }
 }
