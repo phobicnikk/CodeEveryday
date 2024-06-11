bb03cb0ea6dca3a5b9de4c90ac64e01e313ab528
Nikhil Nishane <139981515+phobicnikk@users.noreply.github.com>
2024-06-11 21:56:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (96.54%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,39 +1,24 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for (int j : arr1) {
-            if (hm.containsKey(j)) {
-                hm.put(j, hm.get(j) + 1);
-            } else {
-                hm.put(j, 1);
+        int[] cnt=new int[1001];
+        for(int i:arr1){
+            cnt[i]++;
+        }
+        int[] ans=new int[arr1.length];
+        int i=0;
+        for(int n:arr2){
+            while(cnt[n]>0){
+                ans[i]=n;
+                cnt[n]--;
+                i++;
             }
         }
-        int[] ans = new int[arr1.length];
-        int cnt=0;
-        for (int j : arr2) {
-            while (hm.get(j) > 0) {
-                ans[cnt] = j;
-                cnt++;
-                hm.put(j, hm.get(j) - 1);
+        for(int j=0;j<cnt.length;j++){
+            while(cnt[j]>0){
+                ans[i]=j;
+                cnt[j]--;
+                i++;
             }
-            hm.remove(j);
-        }
-        int[] remaining = new int[arr1.length-cnt];
-        int index = 0;
-        for (int j : arr1) {
-            if (hm.containsKey(j)) {
-                while (hm.get(j) > 0) {
-                    remaining[index] = j;
-                    index++;
-                    hm.put(j, hm.get(j) - 1);
-                }
-                hm.remove(j);
-            }
-        }
-        Arrays.sort(remaining);
-        for (int j : remaining) {
-            ans[cnt] = j;
-            cnt++;
         }
         return ans;
     }
